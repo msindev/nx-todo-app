0d8122071474ee3a8ad0fff07d989f32f3e2053d
mohitsi2 <mohitsi2@cisco.com>
2023-05-28 20:14:48 +0530

    (feat) Add Input and Button and remove UI lib

--- apps/todo/src/app/app.component.ts
+++ apps/todo/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MatTabsModule } from '@angular/material/tabs';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, MatTabsModule],
+  imports: [RouterModule, MatTabsModule, TodoListComponent],
   selector: 'nx-todo-app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
--- apps/todo/src/app/todo-list/todo-list.component.ts
+++ apps/todo/src/app/todo-list/todo-list.component.ts
@@ -1,10 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'nx-todo-app-todo-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCheckboxModule, MatInputModule, MatButtonModule],
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
